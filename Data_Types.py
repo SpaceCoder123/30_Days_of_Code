1a7a4ba07d10311b02bc8ffaba639ba489d4fe38
Mohan Ram T.R <51279552+SpaceCoder123@users.noreply.github.com>
2020-04-25 09:25:36 +0530

    Create Data_Types.py

---
+++ Data_Types.py
@@ -0,0 +1,15 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x=int(input())
+# Read and save an integer, double, and String to your variables.
+y=float(input())
+# Print the sum of both integer variables on a new line.
+print(round(x+i))
+# Print the sum of the double variables on a new line.
+print(y+d)
+# Concatenate and print the String variables on a new lines
+S=input()
+# The 's' variable above should be printed first.
+print('{}{}'.format(s,S))
